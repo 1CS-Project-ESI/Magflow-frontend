c19214f40c6a9ace589d6864da14d270efc522ae
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-03-30 16:31:34 +0100

    modif for role and permissiosn link

--- src/components/tables/permissionsTable.tsx
+++ src/components/tables/permissionsTable.tsx
@@ -4,23 +4,30 @@ import React from "react";
 
 import { PERMISSIONS } from "@/constants";
 
-const PermissionsTable: React.FC = () => {
+interface PermissionsTableProps {
+  permissions: {
+    id: number;
+    name: string;
+  }[];
+}
+
+const PermissionsTable: React.FC<PermissionsTableProps> = ({ permissions = [] }) => {
   return (
     <div className="overflow-x-auto border border-gray-300 rounded-xl m-8">
       <table className="table-auto w-full overflow-hidden">
-        <tbody>
-          {PERMISSIONS.map((perm, index) => (
-            <tr key={index}>
-              <td className="bg-white text-center text-gray-500 flex justify-between px-4 py-5">
-                <div className="ml-14">{perm}</div>
-                <div>
-                  <input type="checkbox" className="form-checkbox bordr h-5 w-5 mx-14" />
-                </div>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <tbody>
+      {permissions.map((permission, index) => (
+        <tr key={permission.id}> 
+          <td className="bg-white text-center text-gray-500 flex justify-between px-4 py-5">
+            <div className="ml-14">{permission.name}</div>
+            <div>
+              <input type="checkbox" className="form-checkbox bordr h-5 w-5 mx-14" />
+            </div>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+   </table>
     </div>
   );
 };
