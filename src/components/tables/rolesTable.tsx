f103048b4b29f229bb3bf353408b9c7f31118971
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-03-31 12:35:02 +0100

    roles perms linking

--- src/components/tables/rolesTable.tsx
+++ src/components/tables/rolesTable.tsx
@@ -21,12 +21,12 @@ const RolesTable: React.FC<RolesTableProps> = ({ roles }) => {
 
   return (
     <div>
-      {roles.map((role, index) => (
+      {roles.map((role) => (
         <div
           key={role.id}
           className="bg-white border border-gray-300 flex justify-between p-6 mb-4 rounded-md"
         >
-          <div key={index}>
+          <div >
             <span className="font-bold text-xl mb-8">{role.name}</span>
             <div className="mb-3">
              
@@ -34,10 +34,12 @@ const RolesTable: React.FC<RolesTableProps> = ({ roles }) => {
           </div>
           <div className="flex items-center">
             <span className="mr-3">
-              {/* Pass role ID to ModifButton */}
-              <ModifButton path="/permissions/"  />  
-              {/* [roleId] */}
-              {/* pathParams={{ roleId: role.id }} */}
+             {/* Construct the URL with query parameters */}
+             <Link href={{ pathname: "/permissions", query: { id: role.id } }}>
+            
+                  <ModifButton />
+              
+              </Link>
             </span>
             {/* <SuppButton /> Assuming SuppButton is implemented */}
           </div>
