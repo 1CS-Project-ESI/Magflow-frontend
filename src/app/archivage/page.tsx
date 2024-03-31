50f090534bea055d4318edbed0f75922862ab42f
AssilOneK <a.kahlerras@esi-sba.dz>
2024-03-31 12:23:18 +0100

    Added buttons

--- src/app/archivage/page.tsx
+++ src/app/archivage/page.tsx
@@ -1,18 +1,32 @@
-//src/app/archivage/page.tsx 
+//src/app/archivage/page.tsx
 "use client";
-import React, { useState, useEffect } from 'react';
-import CreerButton from '@/components/buttons/CreerButton';
-import SelectionnerForm from '../../components/popups/selectionnerForm';
-import BackupTable from '../../components/tables/backupTable';
+import React, { useState, useEffect } from "react";
+import CreerButton from "@/components/buttons/CreerButton";
+import SelectionnerForm from "../../components/popups/selectionnerForm";
+import BackupTable from "../../components/tables/backupTable";
 import { Archive } from "@/types";
 import { BACKUPS } from "@/constants";
 import RootLayout from "../rootLayout";
-import CheckIcon from '../../../public/assets/icons/filter.svg'
+import CheckIcon from "../../../public/assets/icons/filter.svg";
 
 const ArchivePage: React.FC = () => {
   const [files, setFiles] = useState<Archive[]>([]);
   const [selectedTables, setSelectedTables] = useState<string[]>([]);
-  const [tables, setTables] = useState<string[]>(["admin", 'agentserviceachat', 'applicationparams', 'backups', 'consumer', 'director', 'magasinier', 'permissions', 'rolepermissions', 'roles', 'structurereponsable', 'users', 'usersroles']);
+  const [tables, setTables] = useState<string[]>([
+    "admin",
+    "agentserviceachat",
+    "applicationparams",
+    "backups",
+    "consumer",
+    "director",
+    "magasinier",
+    "permissions",
+    "rolepermissions",
+    "roles",
+    "structurereponsable",
+    "users",
+    "usersroles",
+  ]);
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
@@ -40,23 +54,24 @@ const ArchivePage: React.FC = () => {
             <button
               className="bg-gray-200 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded"
               onClick={() => setShowPopup(true)}
-            > v
+            >
+              {" "}
+              v
             </button>
           </div>
           <div className="ml-auto">
-      <CreerButton />
-    </div>
-        </div >
-        <BackupTable Archives={BACKUPS}/>
-        
-  {showPopup && (
-    <SelectionnerForm
-      tables={tables}
-      onSelect={handleTableSelection}
-      onClose={() => setShowPopup(false)}
-    />
-  )}
+            <CreerButton />
+          </div>
+        </div>
+        <BackupTable Archives={BACKUPS} />
 
+        {showPopup && (
+          <SelectionnerForm
+            tables={tables}
+            onSelect={handleTableSelection}
+            onClose={() => setShowPopup(false)}
+          />
+        )}
       </div>
     </RootLayout>
   );
--- src/components/tables/backupTable.tsx
+++ src/components/tables/backupTable.tsx
@@ -15,11 +15,14 @@ interface Props {
         <table className="table-auto w-full overflow-hidden">
           <thead>
             <tr className="bg-white text-zinc-400">
-              <th className="px-4 py-4 font-light">nom</th>
-              <th className="px-4 py-2 font-light hidden md:table-cell">local</th>
-              <th className="px-4 py-2 font-light hidden md:table-cell">date de modification</th>
+              <th className="px-4 py-4 font-light">Nom</th>
+              <th className="px-4 py-2 font-light hidden md:table-cell">Local</th>
+              <th className="px-4 py-2 font-light hidden md:table-cell">Date de modification</th>
             </tr>
           </thead>
+          <tbody>
+            {/* add backups table from back */}
+          </tbody>
         </table>
 
       </div>
