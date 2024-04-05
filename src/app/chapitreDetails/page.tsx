12b4ec6ee99fa1896140170757736b27d3e75b52
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-04-05 05:37:23 +0100

    linking

--- src/app/chapitreDetails/page.tsx
+++ src/app/chapitreDetails/page.tsx
@@ -4,11 +4,14 @@ import AgentLayout from "../agentLayout";
 import { Chapter } from "@/types";
 import { Article } from "@/types";
 import ChapterDetailsTable from "@/components/tables/chapterDetailsTable";
+import getToken from "@/utils/getToken";
 
 interface Props {
   chapters: Chapter[];
   articles: Article[];
 }
+
+// link to getArtcile chapitre to be mapped in chapter details Tables 
 
 const ChapterDetails: React.FC = () => {
   const [chapter, setChapter] = useState<Chapter>({
@@ -16,7 +19,51 @@ const ChapterDetails: React.FC = () => {
     description: " simple description",
     id_agentserviceachat: 0,
   });
-  const [articles, setArticles] = useState<Article[]>([]);
+
+  // link to getArtcile chapitre to be mapped in chapter details Tables 
+
+  const [articlos, setArticles] = useState<Article[]>([]);
+  useEffect(() => {
+    fetchChappArtciles();
+  }, []);
+
+  const fetchChappArtciles = async () => {
+  
+    const accessToken = await getToken();
+    // getting the id from url 
+    const url = new URL(window.location.href);
+        const idString = url.searchParams.get('id');
+
+        let id: number | null = null;
+
+        if (idString !== null) {
+          id = parseInt(idString, 10);
+          
+        }
+        console.log("this id the ",id);
+    try {
+      const response = await fetch(`http://localhost:4000/api/store/chapter/articles/${id}`,{
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+       
+      });
+      if (response.ok) {
+        const data = await response.json();
+        const articlos = data;
+        console.log(articlos);
+      
+        setArticles(articlos);
+  
+      } else {
+        console.error("Failed to fetch chapp artciles :", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching arctiles:", error);
+    }
+  };
+
 
   return (
     <AgentLayout>
@@ -28,7 +75,7 @@ const ChapterDetails: React.FC = () => {
           Description :<span className="font-bold">{chapter.description}</span>
         </div>
         <div className="text-xl mb-4">Articles :</div>
-        <ChapterDetailsTable articles={articles} />
+        <ChapterDetailsTable articles={articlos} />
       </div>
     </AgentLayout>
   );
--- src/components/tables/chapterTable.tsx
+++ src/components/tables/chapterTable.tsx
@@ -1,11 +1,9 @@
-"use client";
 
 import React from "react";
-
 import Link from "next/link";
-
 import UpdateChapterButton from "../buttons/updateChapterButton";
-import dlt from "../../../public/assets/icons/delete.svg";
+import deleteIcon from "../../../public/assets/icons/delete.svg";
+import getToken from "@/utils/getToken";
 
 interface Chapter {
   id: number;
@@ -18,28 +16,64 @@ interface ChaptersTableProps {
   chapters: Chapter[];
 }
 
+
+const handleDeleteChapter = async (id: number) => {
+ 
+  const accessToken = await getToken();
+
+  try {
+    const response = await fetch(
+      `http://localhost:4000/api/store/chapter/delete/${id}`,
+      {
+        method: "DELETE", // Set request method to DELETE
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error deleting structure: ${await response.text()}`);
+    }
+
+    console.log("chapter deleted successfully!");
+  
+  } catch (error) {
+    console.error("Error during deletion:", error);
+
+  } finally {
+   
+  }
+};
+
 const ChaptersTable: React.FC<ChaptersTableProps> = ({ chapters }) => {
   return (
     <div>
-      {chapters.map((chp) => (
+      {chapters.map((chapter) => (
         <div
-          key={chp.id}
+          key={chapter.id}
           className="bg-white border border-gray-300 flex justify-between p-6 mb-4 rounded-md"
         >
-          <Link
-            href=""
-          >
-            <div key={chp.id}>
-              <span className="font-bold text-xl mb-8">{chp.name}</span>
-            </div>
-          </Link>
-          <div className="flex items-center">
-            <span className="mr-3">
-              <UpdateChapterButton showPopup={true} />
+          <div>
+            <span className="font-bold text-xl mb-8">
+              <Link href={{ pathname: "/chapitreDetails", query: { id: chapter.id } }}>
+                {chapter.name}
+              </Link>
             </span>
-            <button className="w-36 bg-transparent border-black border-2 hover:bg-black hover:text-white font-medium py-2 px-4 rounded-lg">
+          </div>
+          <div className="flex items-center">
+            {/* <span className="mr-3">
+              <UpdateChapterButton showPopup={true} />
+            </span> */}
+            <button
+                className="w-36 bg-transparent border-black border-2 hover:bg-black hover:text-white font-medium py-2 px-4 rounded-lg"
+                onClick={async () => {
+                  await handleDeleteChapter(chapter.id);
+                }}
+              >
               <div className="flex items-center space-x-2">
-                <img src={dlt.src} width="18" height="15" />
+                <img src={deleteIcon.src} alt="delete" width="18" height="15" />
                 <span>Supprimer</span>
               </div>
             </button>
@@ -51,3 +85,4 @@ const ChaptersTable: React.FC<ChaptersTableProps> = ({ chapters }) => {
 };
 
 export default ChaptersTable;
+
