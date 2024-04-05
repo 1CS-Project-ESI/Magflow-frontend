cf832ca8cad86f951aed35c8a9b92ef16a36564e
AsmaaBaiche <a.baiche@esi-sba.dz>
2024-04-04 15:48:46 -1200

    commandes page done

--- src/app/commandes/page.tsx
+++ src/app/commandes/page.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+"use client";
 
+import React, { useEffect, useState } from "react";
+import AddCommandeButton from "@/components/buttons/addCommandeButton";
+import CommandesTable from "@/components/tables/commandesTable";
 import AgentLayout from "../agentLayout";
 
-const CommandesPage: React.FC = () => (
+
+const CommandesPage: React.FC = () => {
+
+  const [commandes, setArticles] = useState([]);
+return (
   <AgentLayout>
-    <div className="text-4xl font-bold">
-      Commandes
-    </div>
+   <div className="flex items-center justify-between">
+  <div className="text-4xl font-bold ml-10">
+    BCE
+  </div>
+  <div className="flex m-8 justify-end">
+    <AddCommandeButton />
+  </div>
+</div>
+      <div className="m-8 mt-8">
+        <CommandesTable commandes={commandes} />
+      </div>
   </AgentLayout>
-);
+
+)
+};
 
 export default CommandesPage;
---
+++ src/components/tables/commandesTable.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React from "react";
+import { Commande } from "@/types";
+import dlt from "../../../public/assets/icons/delete.svg"
+
+interface Props {
+  commandes: Commande[];
+}
+
+const CommandesTable: React.FC<Props> = ({ commandes }) => {
+  return (
+    <div className="overflow-x-auto border border-gray-300 rounded-xl">
+      <table className="table-auto w-full overflow-hidden">
+        <thead>
+          <tr className="bg-white text-zinc-400">
+            <th className="px-4 py-4 font-light">ID_de_bon</th>
+            <th className="px-4 py-2 font-light hidden md:table-cell">
+              Date
+            </th>
+            <th className="px-4 py-2 font-light hidden md:table-cell">
+              Etat
+            </th>
+            <th className="px-4 py-2 font-light hidden md:table-cell"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {commandes.map((commande, index) => (                       //boucle 
+            <tr key={index} className="border-b border-gray-200">
+              <td className="border-t bg-white text-center px-4 py-4">
+                {commande.ID_de_bon}
+              </td>
+              <td className="border-t bg-white text-center px-4 py-2 hidden md:table-cell">
+                {commande.Date}
+              </td>
+              <td className="border-t bg-white text-center px-4 py-2 hidden md:table-cell">
+                {commande.Etat}
+              </td>
+              <td className="border-t bg-white text-center px-4 py-2 md:table-cell flex items-center justify-center">
+                <button
+                  className="w-36 bg-transparent border-black border-2 hover:bg-black hover:text-white font-medium py-2 px-4 rounded-lg"
+                >
+                  <div className="flex items-center space-x-2">
+                    <img src={dlt.src} width="18" height="15" />
+                    <span>Supprimer</span>
+                  </div>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CommandesTable;
