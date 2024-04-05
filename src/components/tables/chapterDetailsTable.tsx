e3ece4c799e32a506b0f85c307b079b089723cb6
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-04-05 06:17:42 +0100

    delete linking

--- src/components/tables/chapterDetailsTable.tsx
+++ src/components/tables/chapterDetailsTable.tsx
@@ -2,12 +2,52 @@
 
 import React, { useState } from "react";
 import { Article } from "@/types";
+import UpdateChapterButton from "../buttons/updateChapterButton";
+import deleteIcon from "../../../public/assets/icons/delete.svg";
+import getToken from "@/utils/getToken";
+
+
+interface artcile {
+  id: number;
+  name: string;
+  description: string;
+  id_agentserviceachat: number;
+}
 
 interface Props {
     articles: Article[];
   }
 
+ // delete artcile if empty
+ const handleDeleteArctile = async (id?: number) => {
  
+  const accessToken = await getToken();
+
+  try {
+    const response = await fetch(
+      `http://localhost:4000/api/store/article/delete/${id}`,
+      {
+        method: "DELETE", // Set request method to DELETE
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error deleting structure: ${await response.text()}`);
+    }
+
+    console.log("chapter deleted successfully!");
+  
+  } catch (error) {
+    console.error("Error during deletion:", error);
+
+  } finally {
+   
+  }
+};
 
 
   
@@ -38,6 +78,20 @@ interface Props {
                   <td className="border-t bg-white text-center px-4 py-2 hidden md:table-cell">
                     {article.chapter_id}
                   </td>
+                  <td>
+                  <button
+                className="w-36 bg-transparent border-black border-2 hover:bg-black hover:text-white font-medium py-2 px-4 rounded-lg"
+                onClick={async () => {
+                 await handleDeleteArctile(article.id);
+                }}
+              >
+              <div className="flex items-center space-x-2">
+                <img src={deleteIcon.src} alt="delete" width="18" height="15" />
+                <span>Supprimer</span>
+              </div>
+            </button>
+                  </td>
+
                 </tr>
               ))}
             </tbody>
