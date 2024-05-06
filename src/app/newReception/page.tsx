b12caa525ff9f5e1b7519cc8d93f6293c409e204
Th3-l4dy <f.bencharef@esi-sba.dz>
2024-05-06 21:30:15 +0100

    jncp

--- src/app/newReception/page.tsx
+++ src/app/newReception/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import React, { useState, useEffect } from "react";
@@ -20,38 +19,9 @@ const Page = () => {
     }[]
   >([]);
 
-  // useEffect(() => {
-  //   fetchCommands();
-  // }, []);
-
-  // const fetchCommands = async () => {
-  //   try {
-  //     const response = await fetch("http://localhost:4000/api/bons/allcommands");
-  //     const data = await response.json();
-  //     if (response.ok) {
-  //       setCommands(data.commands);
-  //     } else {
-  //       console.error("Error fetching commands:", response.statusText);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error fetching commands:", error);
-  //   }
-  // };
-  
-  // const handleSelectCommand = (command: Commande | null) => {
-  //   if (command) {
-  //     console.log("this is the selected commands  command.id",command.id);
-  //     fetchCommandProducts(command.id.toString());
-  //   } else {
-  //     setProducts([]);
-  //   }
-  // };
-
   useEffect(() => {
-      fetchCommandProducts();
-    }, []);
-  
-
+    fetchCommandProducts();
+  }, []);
 
   const fetchCommandProducts = async () => {
     const accessToken = await getToken();
@@ -63,10 +33,12 @@ const Page = () => {
       id = parseInt(idString, 10);
     }
     try {
-      const response = await fetch(`http://localhost:4000/api/bons/commandproducts/${id}`);
+      const response = await fetch(
+        `http://localhost:4000/api/bons/commandproducts/${id}`
+      );
       if (response.ok) {
         const data = await response.json();
-        console.log("this is the data.products",data.products);
+        console.log("this is the data.products", data.products);
         setProducts(data.products);
       } else {
         console.error("Failed to fetch command products:", response.statusText);
@@ -82,90 +54,105 @@ const Page = () => {
   const handleDeliveryQuantityChange = (index: number, value: number) => {
     setDeliveryQuantities((prevQuantities) => {
       const newQuantities = [...prevQuantities];
-      newQuantities[index] = value; // Update quantity at specific index
+      newQuantities[index] = value;
       return newQuantities;
     });
   };
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const accessToken = await getToken();
-    const id = await UserID();
+    const id_magasinier = await UserID();
 
     const now = new Date();
     const orderdate = now.toISOString().substring(0, 10);
+
     const randomNumber = Math.floor(Math.random() * 900000) + 100000;
     const number = `${randomNumber}`;
+
     const DeliveryDate = new Date(now);
     DeliveryDate.setDate(DeliveryDate.getDate());
     const deliverydate = DeliveryDate.toISOString().substring(0, 10);
 
-    
-    // const products = selectedOptions.map((option) => option.product?.id);
-    const products = selectedOptions
-  .filter((option) => option.product !== undefined && option.product !== null)
-  .map((option) => option.product?.id);
-  //  const deliveryQuantities = selectedOptions.map((option) => option.deliveredQuantity);
-     console.log("the user with is has a magatiner role",id);
-    console.log("this is the products array ",products);
-    console.log("this is receivedQuantities",deliveryQuantities);
-    
-    const formData = {
-      number,
-      deliverydate,
-      products,
-      deliveryQuantities,
-    };
+    const url = new URL(window.location.href);
+    const idString = url.searchParams.get("id");
+    let id_boncommande = null;
 
+    if (idString !== null) {
+      id_boncommande = parseInt(idString, 10);
 
-    console.log("thhis is the formData",formData);
-    try {
-      const response = await fetch(`http://localhost:4000/api/bons/create-bon-reception/${id}`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData), // Use formData here
-      });
+      const products = selectedOptions
+        .filter(
+          (option) => option.product !== undefined && option.product !== null
+        )
+        .map((option) => option.product?.id);
 
+      console.log("the user with is has a magatiner role", id_magasinier);
+      console.log("this is the products array ", products);
+      console.log("this is receivedQuantities", deliveryQuantities);
 
+      const formData = {
+        id_magasinier,
+        number,
+        deliverydate,
+        products,
+        deliveryQuantities,
+      };
 
-      if (response.ok) {
-        console.log("Commandes added successfully");
-        // Add any additional success handling here
-      } else {
-        console.error("Error adding commandes:", response.statusText);
+      console.log("thhis is the formData", formData);
+      try {
+        const response = await fetch(
+          `http://localhost:4000/api/bons/create-bon-reception/${id_boncommande}`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+
+        if (response.ok) {
+          console.log("bon reception  added successfully");
+        } else {
+          console.error("Error adding bon reception :", response.statusText);
+        }
+      } catch (error) {
+        console.error("Error adding bon de reception :", error);
       }
-    } catch (error) {
-      console.error("Error adding commandes:", error);
     }
   };
-
-return (
-  <RootLayout>
-    <div className="bg-white border border-gray-300 grid grid-cols-1 p-8 m-8 rounded-md">
-      <h1 className="text-2xl mx-8">Nouvelle Reception</h1>
-      <OptionSelection
+  return (
+    <RootLayout>
+      <div className="bg-white border border-gray-300 grid grid-cols-1 p-8 m-8 rounded-md">
+        <h1 className="text-2xl mx-8">Nouvelle Reception</h1>
+        <OptionSelection
           products={products}
           selectedOptions={selectedOptions}
-          setSelectedOptions={setSelectedOptions} // Pass the prop here
+          setSelectedOptions={setSelectedOptions}
           onDeliveryQuantityChange={handleDeliveryQuantityChange}
         />
 
-      <div className="w-full flex justify-end">
-        <button
-          className="bg-purple-950 text-white hover:bg-black font-medium py-2 px-4 mx-8 rounded-lg"
-          onClick={handleSubmit}
-        >
-          <div className="flex items-center space-x-2">
-            <img src={save.src} width="18" height="18" style={{ filter: "invert(100%)" }} alt="Save" />{" "}
-            <span>Enregistrer</span>
-          </div>
-        </button>
+        <div className="w-full flex justify-end">
+          <button
+            className="bg-purple-950 text-white hover:bg-black font-medium py-2 px-4 mx-8 rounded-lg"
+            onClick={handleSubmit}
+          >
+            <div className="flex items-center space-x-2">
+              <img
+                src={save.src}
+                width="18"
+                height="18"
+                style={{ filter: "invert(100%)" }}
+                alt="Save"
+              />{" "}
+              <span>Enregistrer</span>
+            </div>
+          </button>
+        </div>
       </div>
-    </div>
-  </RootLayout>
-);
+    </RootLayout>
+  );
 };
 
 export default Page;
